1620cf35c8ae9e8f48b48ea8501ddbb7e2dac914
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-02-18 19:32:27 -0300

    Installed CORS on the backend to integrate with the frontend

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const routes = require("./routes/index");
 const express = require("express");
 const app = express();
+const cors = require("cors")
 
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 
 const PORT = 5555;
