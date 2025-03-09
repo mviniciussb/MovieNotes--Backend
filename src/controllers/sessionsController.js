1df8b14d0df9f9c0ee042e314187877639a3d238
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-09 15:17:09 -0300

    :)

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,33 +1,38 @@
 const knex = require("../database/knex/index");
 const { compare } = require("bcryptjs");
-const jwt  = require ("../config/auth.js")
-const pkg = require ("jsonwebtoken"); 
+const jwt = require("../config/auth.js"); // Configuração do JWT
+const pkg = require("jsonwebtoken");
 const { sign } = pkg;
 
 class SessionsController {
     async create(request, response) {
+        const { email, password } = request.body;
 
-        const { email, password } = request.body
-        const user = await knex("users").where({ email }).first()
+        // Verifica se o usuário existe
+        const user = await knex("users").where({ email }).first();
 
         if (!user) {
-            return response.status(401).json({ Error: "E-mail ou senha incorreta." });
+            return response.status(401).json({ error: "E-mail ou senha incorreta." });
         }
 
-        const passwordMatched = await compare(password, user.password)
+        // Compara a senha fornecida com a senha armazenada no banco
+        const passwordMatched = await compare(password, user.password);
 
         if (!passwordMatched) {
-            return response.status(401).json({ Error: "E-mail ou senha incorreta." });
+            return response.status(401).json({ error: "E-mail ou senha incorreta." });
         }
 
-        const { secret, expiresIn } = jwt
-        const token = sign({}, secret, {
-            subject: String(user.id),
-            expiresIn
-        })
+        // Configuração do JWT
+        const { secret, expiresIn } = jwt;
 
-        return response.json({ user, token })
+        // Cria o token
+        const token = sign({}, secret, {
+            subject: String(user.id),  // A chave 'subject' é o ID do usuário
+            expiresIn,  // Define o tempo de expiração do token
+        });
+
+        return response.json({ user, token }); // Retorna o usuário e o token
     }
 }
 
-module.exports = SessionsController 
+module.exports = SessionsController;
