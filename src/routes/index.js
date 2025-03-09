cae601cfe7bb5f72e1b03d6bfd81998c49773954
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-09 14:24:47 -0300

    sessions routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const userRoutes = require("./usersRoutes");
 const notesRoutes = require("./notesRoutes");
 const tagsRoutes = require("./tagsRoutes");
+const sessionsRoutes = require("./sessionsRoutes");
+
 
 const { Router } = require("express");
 const routes = Router();
@@ -8,6 +10,7 @@ const routes = Router();
 routes.use("/users", userRoutes);
 routes.use("/notes", notesRoutes);
 routes.use("/tags", tagsRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 
 module.exports = routes;
---
+++ src/routes/sessionsRoutes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express")
+const { SessionsController } = require ("../controllers/sessionsController")
+const appSessionsRouters = Router()
+
+const sessionsController = new SessionsController()
+
+appSessionsRouters.post("/", sessionsController.create)
+
+export default appSessionsRouters
