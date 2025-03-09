ae7664c7da6dcc4b7be4631e7df8511f75530ac9
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-09 14:19:13 -0300

    Update sessionsController.js

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,3 +1,30 @@
 const knex = require("../database/knex/index");
 const { compare } = require("bcryptjs");
 
+class SessionsController {
+    async create(request, response) {
+
+        const { email, password } = request.body
+        const user = await knex("users").where({ email }).first()
+
+        if (!user) {
+            throw new AppError("E-mail e/ou senha incorreta.", 401)
+        }
+
+        const passwordMatched = await compare(password, user.password)
+
+        if (!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreta.", 401)
+        }
+
+        const { secret, expiresIn } = jwt
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+
+        return response.json({ user, token })
+    }
+}
+
+export { SessionsController }
